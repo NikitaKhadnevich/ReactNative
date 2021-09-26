23c6a6ab1e975ed84ea0f919061e3dc403d9a71a
NikitaKhadnevich <vadim-swich@yandex.ru>
2021-09-26 15:22:05 +0300

    Add some fix

--- SmartTodo-Demo/src/AddTodo.js
+++ SmartTodo-Demo/src/AddTodo.js
@@ -40,7 +40,7 @@ const styles = StyleSheet.create({
       width: '70%',
       borderStyle: 'solid',
       borderBottomColor: 'silver',
-      borderBottomWidth: 1,
+      borderBottomWidth: 2,
       color: 'silver',
    },
    addButtom: {
